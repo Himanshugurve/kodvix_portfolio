123d4ec21e27d9f6921e70e2d1fecc11c830fb3b
hrishikapandey <hrishiap38@gmail.com>
2026-02-23 12:45:04 +0530

    ui fixing done for mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ export default function App() {
           id={id}
           style={{
             width: '100vw',
-            // height: '100vh',
+            height: id === 'cover' ? '100vh' : 'auto',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
